afbceeee9668453a7248a0d9c9b5383dbd7d11f9
Eiji Tachikawa <tsuyoku1202@gmail.com>
2024-08-01 10:08:43 +0900

    feat: eslintやmetadataの設定 Fixes #1

---
+++ config/site.ts
@@ -0,0 +1,26 @@
+import { SiteConfig } from "@/types";
+
+
+export const siteConfig: SiteConfig = {
+  name: "RecipeAdviser",
+  description: "冷蔵庫の中にある食材から料理を提案してくれるアプリです",
+  url: "http://localhost:3000",
+  ogImage: "",
+  links: {
+    x: "https://x.com/ET_1202",
+    github: "https://github.com/Eiji1202",
+  },
+  keywords: [
+    "Next.js",
+    "TypeScript",
+    "TailwindCSS",
+    "Supabase",
+    "OpenAI API",
+  ],
+  authors: [
+    {
+      name: "Eiji",
+      url: "http://localhost:3000",
+    },
+  ],
+};
